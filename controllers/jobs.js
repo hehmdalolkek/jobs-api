d037b21a8c82b35302d6429cf5fb3b24cf9c37da
hehmdalolkek <badeha.inna@yandex.ru>
2023-10-27 16:33:08 +0400

    added Job model and jobs controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,21 +1,89 @@
+const Job = require('../models/Job');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, NotFoundError } = require('../errors/index');
+
+
 const getAllJobs = async (req, res) => {
-	res.send('Get all jobs');
+	const userId = req.user.userId
+	const jobs = await Job.find({ createdBy: userId }).sort('createdAt');
+
+	res
+		.status(StatusCodes.OK)
+		.json({ count: jobs.length, jobs });
 };
 
 const getJob = async (req, res) => {
-	res.send('Get job');
+	const userId = req.user.userId;
+	const jobId = req.params.id;
+
+	const job = await Job.findOne({ 
+		_id: jobId,
+		createdBy: userId
+	});
+
+	if (!job) {
+		throw new NotFoundError(`No job with id: ${jobId}`);
+	}
+
+	res
+		.status(StatusCodes.OK)
+		.json({ job });
 };
 
 const createJob = async (req, res) => {
-	res.send('Create job');
+	req.body.createdBy = req.user.userId;
+
+	const job = await Job.create(req.body);
+
+	res
+		.status(StatusCodes.CREATED)
+		.json({ job });
 };
 
 const updateJob = async (req, res) => {
-	res.send('Update job');
+	const {
+		body: { company, position },
+		user: { userId },
+		params: { id: jobId }
+	} = req;
+
+	if (!company === '' || !position === '') {
+		throw new BadRequestError('Company and position fields cannot be empty');
+	}
+
+	const job = await Job.findByIdAndUpdate(
+		{	_id: jobId, createdBy: userId },
+		req.body, 
+		{ new: true, runValidators: true }
+	);
+
+	if (!job) {
+		throw new NotFoundError(`No job with id: ${jobId}`);
+	}
+
+	res
+		.status(StatusCodes.OK)
+		.json({ job });
 };
 
 const deleteJob = async (req, res) => {
-	res.send('Delete job');
+	const {
+		user: { userId },
+		params: { id: jobId }
+	} = req;
+
+	const job = await Job.findByIdAndRemove({
+		_id: jobId,
+		createdBy: userId
+	});
+
+	if (!job) {
+		throw new NotFoundError(`No job with id: ${jobId}`);
+	}
+
+	res
+		.status(StatusCodes.OK)
+		.send();
 };
 
 
