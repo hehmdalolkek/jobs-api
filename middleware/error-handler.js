f6cf6e6fea691ebe9834dd7367ad085b1263fb7d
hehmdalolkek <badeha.inna@yandex.ru>
2023-10-27 18:09:22 +0400

    changing and adding new errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,14 +1,34 @@
-const { CustomAPIError } = require('../errors/index');
 const { StatusCodes } = require('http-status-codes');
 
 
 const errorHandler = async (err, req, res, next) => {
-	if (err instanceof CustomAPIError) {
-		return res.status(err.statusCode).json({ msg: err.message });
+	let customError = {
+		statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+		msg: err.message || 'Something went wrong, try again'
+	};
+	
+
+	if (err.name === 'ValidationError') {
+		customError.msg = Object.values(err.errors)
+			.map(item => item.message)
+			.join(', ');
+		customError.statusCode = StatusCodes.BAD_REQUEST;
 	}
+
+	if (err.code && err.code === 11000) {
+		customError.statusCode = StatusCodes.BAD_REQUEST;
+		customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field`;
+	}
+
+	if (err.name === 'CastError') {
+		customError.msg = `No item found with id: ${err.value}`;
+		customError.statusCode = StatusCodes.NOT_FOUND;
+	}
+
+
 	res
-		.status(StatusCodes.INTERNAL_SERVER_ERROR)
-		.json({ err });
+		.status(customError.statusCode)
+		.json(customError.msg);
 };
 
 
