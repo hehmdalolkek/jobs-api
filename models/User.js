19c1f4106b246b3759c13bfdcba39cc9a2f7e904
hehmdalolkek <badeha.inna@yandex.ru>
2023-10-27 18:07:29 +0400

    fixed typo in User.js

--- models/User.js
+++ models/User.js
@@ -7,13 +7,13 @@ const jwt = require('jsonwebtoken');
 const UserSchema = new mongoose.Schema({
 	name: {
 		type: String,
-		require: [true, 'Please provide name'],
+		required: [true, 'Please provide name'],
 		minlength: 3,
 		maxlength: 50
 	},
 	email: {
 		type: String,
-		require: [true, 'Please provide email'],
+		required: [true, 'Please provide email'],
 		match: [
 			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 			'Please valid provide email'
@@ -22,7 +22,7 @@ const UserSchema = new mongoose.Schema({
 	},
 	password: {
 		type: String,
-		require: [true, 'Please provide email'],
+		required: [true, 'Please provide password'],
 		minlength: 6
 	}
 });
