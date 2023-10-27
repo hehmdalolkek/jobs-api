9bfe06a7bdee62017babc4c65c15ccafe53e195e
hehmdalolkek <badeha.inna@yandex.ru>
2023-10-27 23:31:31 +0400

    swagger docs added

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// swagger
+const YAML = require('yamljs');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -35,8 +40,10 @@ app.use(rateLimiter({
 
 // routes
 app.get('/', (req, res) => {
-	res.send('jobs api');
+	res.send('<h1>Jobs API</h1><a href="./api-docs">Documentation</a>');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
